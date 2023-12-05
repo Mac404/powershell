445b7fde6b3e8f9d44a6fde7d48b5538cf2c8f07
Mac404 <macsf1@gmail.com>
2023-12-05 13:48:36 -0800

    Testing and adding step2 consisting of 3 commands

--- sys-admin.ps1
+++ sys-admin.ps1
@@ -6,8 +6,18 @@
 # 4. Write a script that creates 7 groups and puts at least one users in each group from the class.
 # 5. The groups should be named IT, HR, DevOps, Sales, Marketing, Finance, and Management.
 
-#1. Renaming Windows Server from EC2AMAZ-DINR4IQ to Thanos
+# 1. Renaming Windows Server from EC2AMAZ-DINR4IQ to Thanos
 
 Rename-Computer -NewName "Thanos" -Restart
+
+# 2. Installing AD Management Tools
+
+Add-WindowsFeature RSAT-AD-PowerShell
+
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+# the next command requires strong pw of uppper/lower/alphanum/sp.chars & after it's successful, it will force a restart
+
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath “C:\Windows\NTDS” -DomainMode “Win2012R2” -DomainName “sia.net” -DomainNetbiosName “DOMAIN_NAME” -ForestMode “Win2012R2” -InstallDns:$true -LogPath “C:\Windows\NTDS” -NoRebootOnCompletion:$false -SysvolPath “C:\Windows\SYSVOL” -Force:$true
 
 
